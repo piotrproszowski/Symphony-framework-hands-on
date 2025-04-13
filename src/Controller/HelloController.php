d88119f7bd4c5c2711bbc3ad87268fd303eeb77d
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2025-04-14 00:30:56 +0200

    feat(HelloController): enhance message retrieval with limit and add showOne route

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,8 +6,19 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HelloController {
 
-  public function index(): Response {
-    return new Response('Hello World');
+  private array $messages = [
+    "hi", "hello", "hey", "how are you", "goodbye"
+  ];
+
+
+  #[Route('/{limit<\d+>?3}', name: 'app_index')]
+  public function index(int $limit): Response {
+    return new Response(implode(',', array_slice($this->messages, 0, $limit)));
+  }
+
+  #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+  public function showOne($id): Response {
+    return new Response($this->messages[$id]);
   }
 
 }
